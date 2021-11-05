9eaba4eb53da14bb9f4c1106397376480cd2489a
t29mato <tomoya.matou@gmail.com>
2021-11-05 01:40:21 +0000

    fix: show material design icon doesn't show

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib/framework'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify)
 
-export const options = {}
+export const options = {
+  icons: {
+    iconfont: 'mdi',
+  },
+}
 
-export default new Vuetify(options)
+export default new Vuetify({ options })
