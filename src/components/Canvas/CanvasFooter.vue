d4f74584adba629c847d0bedc1fd164963be7737
t29mato <tomoya.matou@gmail.com>
2022-08-17 07:07:06 +0000

    feat: added same as x1 button

--- src/components/Canvas/CanvasFooter.vue
+++ src/components/Canvas/CanvasFooter.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="mt-2">
-    <v-btn small :disabled="!axes.exist" @click="clickAxes"> Clear Axes</v-btn>
+    <v-btn small :disabled="axes.nextIndex !== 2" @click="sameAsX1"
+      >Same as X1</v-btn
+    >
+    <v-btn class="ml-2" small :disabled="!axes.exist" @click="clickAxes">
+      Clear Axes</v-btn
+    >
     <v-btn
       small
       class="ml-2"
@@ -34,11 +39,14 @@ export default Vue.extend({
   },
   methods: {
     ...datasetMapper.mapActions(['clearPlots', 'clearActivePlots']),
-    ...axesMapper.mapActions(['clearAxes']),
+    ...axesMapper.mapActions(['clearAxes', 'addAxis']),
     ...canvasMapper.mapActions(['setManualMode']),
     clickAxes() {
       this.clearAxes()
       this.setManualMode(-1)
+    },
+    sameAsX1() {
+      this.addAxis(this.axes.x1)
     },
   },
 })
