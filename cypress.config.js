696ef56a37df5f616c1d707eba2893505ddeca1d
t29mato <tomoya.matou@gmail.com>
2023-01-11 00:44:24 +0000

    chore: add cypree config and sample tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   projectId: 'jjruua',
-
   // The rest of the Cypress config options go here...
+  viewportWidth: 1280,
+  viewportHeight: 700,
   e2e: {
     baseUrl: 'http://localhost:8080',
     setupNodeEvents(on, config) {
