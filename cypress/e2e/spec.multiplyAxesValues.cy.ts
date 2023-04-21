49fbf7c7063ca6870de4bfd3a17e8b45bd2049e0
t29mato <tomoya.matou@gmail.com>
2023-04-21 17:01:38 +0900

    refactor: test code

--- cypress/e2e/spec.multiplyAxesValues.cy.ts
+++ cypress/e2e/spec.multiplyAxesValues.cy.ts
@@ -1,53 +1,62 @@
 /* eslint-disable jest/expect-expect */
-// INFO: Cypressのshould methodでアサーションは実質、実施してるので
 describe('template spec', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-  it('should enable x10 and x0.1 buttons when Log checkbox is checked', () => {
-    cy.get('#multiply-by-ten-x1').should('not.exist')
-    cy.get('#divide-by-ten-x1').should('not.exist')
-    // Log checkboxをクリック
-    cy.get('#x-is-log').click({ force: true })
 
-    // x10 buttonをクリックして値が10倍になることを確認
-    cy.get('#x1-value').type('{selectall}2').should('have.value', '2')
-    cy.get('#multiply-by-ten-x1').click({ force: true })
-    cy.get('#x1-value').should('have.value', '2e+1')
-    cy.get('#x2-value').type('{selectall}5').should('have.value', '5')
-    cy.get('#multiply-by-ten-x2').click()
-    cy.get('#x2-value').should('have.value', '5e+1')
+  describe('X軸', () => {
+    it('should enable x10 button when Log checkbox is checked and x1 value is multiplied by 10', () => {
+      // Arrange
+      cy.get('#multiply-by-ten-x1').should('not.exist')
+      cy.get('#x-is-log').click({ force: true })
 
-    // x0.1 buttonをクリックして値が0.1倍になることを確認
-    cy.get('#x1-value').type('{selectall}2').should('have.value', '2')
-    cy.get('#divide-by-ten-x1').click({ force: true })
-    cy.get('#x1-value').should('have.value', '0.2')
-    cy.get('#x2-value').type('{selectall}5').should('have.value', '5')
-    cy.get('#divide-by-ten-x2').click()
-    cy.get('#x2-value').should('have.value', '0.5')
+      // Act
+      cy.get('#x1-value').type('{selectall}2').should('have.value', '2')
+      cy.get('#multiply-by-ten-x1').click({ force: true })
 
-    cy.get('#x-is-log').click({ force: true })
-    cy.get('#multiply-by-ten-x1').should('not.exist')
-    cy.get('#divide-by-ten-x1').should('not.exist')
+      // Assert
+      cy.get('#x1-value').should('have.value', '2e+1')
+    })
 
-    cy.get('#multiply-by-ten-y1').should('not.exist')
-    cy.get('#divide-by-ten-y1').should('not.exist')
-    cy.get('#y-is-log').click({ force: true })
-    // y軸に関してもx軸と同様のテストを実施
-    cy.get('#y1-value').type('{selectall}2').should('have.value', '2')
-    cy.get('#divide-by-ten-y1').click({ force: true })
-    cy.get('#y1-value').should('have.value', '0.2')
-    cy.get('#y2-value').type('{selectall}5').should('have.value', '5')
-    cy.get('#divide-by-ten-y2').click()
-    cy.get('#y2-value').should('have.value', '0.5')
-    cy.get('#y1-value').type('{selectall}2').should('have.value', '2')
-    cy.get('#multiply-by-ten-y1').click({ force: true })
-    cy.get('#y1-value').should('have.value', '2e+1')
-    cy.get('#y2-value').type('{selectall}5').should('have.value', '5')
-    cy.get('#multiply-by-ten-y2').click()
-    cy.get('#y2-value').should('have.value', '5e+1')
-    cy.get('#y-is-log').click({ force: true })
-    cy.get('#multiply-by-ten-y1').should('not.exist')
-    cy.get('#divide-by-ten-y1').should('not.exist')
+    it('should enable x0.1 button when Log checkbox is checked and x1 value is divided by 10', () => {
+      // Arrange
+      cy.get('#divide-by-ten-x1').should('not.exist')
+      cy.get('#x-is-log').click({ force: true })
+
+      // Act
+      cy.get('#x1-value').type('{selectall}2').should('have.value', '2')
+      cy.get('#divide-by-ten-x1').click({ force: true })
+
+      // Assert
+      cy.get('#x1-value').should('have.value', '0.2')
+    })
+  })
+
+  describe('Y軸', () => {
+    it('should enable y10 button when Log checkbox is checked and y1 value is multiplied by 10', () => {
+      // Arrange
+      cy.get('#multiply-by-ten-y1').should('not.exist')
+      cy.get('#y-is-log').click({ force: true })
+
+      // Act
+      cy.get('#y1-value').type('{selectall}2').should('have.value', '2')
+      cy.get('#multiply-by-ten-y1').click({ force: true })
+
+      // Assert
+      cy.get('#y1-value').should('have.value', '2e+1')
+    })
+
+    it('should enable y0.1 button when Log checkbox is checked and y1 value is divided by 10', () => {
+      // Arrange
+      cy.get('#divide-by-ten-y1').should('not.exist')
+      cy.get('#y-is-log').click({ force: true })
+
+      // Act
+      cy.get('#y1-value').type('{selectall}2').should('have.value', '2')
+      cy.get('#divide-by-ten-y1').click({ force: true })
+
+      // Assert
+      cy.get('#y1-value').should('have.value', '0.2')
+    })
   })
 })
