9495ae23aa37cd4c332c747acd50a3fb5aa80887
MATO Tomoya <tomoya.matou@gmail.com>
2023-04-26 19:48:09 +0900

    Feature/add tabular to edit dataset values (#10)
    * feat: add table to edit data of dataset
    
    * chore: bump version for some libraries which have vulnerability
    
    * chore: remove Clipboard.vue because we've replaced to DataTable.vue
    
    * chore: add ts-ignore, some of them are apparently defined though
    
    * chore: fix TODO comments
    
    * refactor: improve readability
    
    * test: add e2e testcase for DataTable component
    
    * chore: fix lint error
    
    * fix: -infinity.toPrecision() get error, because Math.log10(0) is -infinity
    
    * fix: xy data is not updated when xy axes values change
    
    * style: make much space for the count of data of dataset on datasets list
    
    * fix: log scale didn't work because the isLog property of the axes's store wasn't updated
    
    * fix: key should be primitive value
    
    * refactor: to make debug easy
    
    * fix: effective digits is supposed to be more than 1

--- src/components/DatasetManager/DatasetManager.vue
+++ src/components/DatasetManager/DatasetManager.vue
@@ -2,136 +2,64 @@
   <div>
     <h4>
       Datasets
-      <v-btn @click="addDataset" small class="ml-2"
+      <v-btn @click="addDataset" x-small class="ml-2"
         ><v-icon>mdi-plus</v-icon></v-btn
       >
       <v-btn
-        small
+        x-small
         @click="popDataset"
         :disabled="this.datasets.datasets.length === 1"
         class="ml-2"
         ><v-icon>mdi-minus</v-icon></v-btn
       >
     </h4>
-    <v-card class="mx-auto mt-2" flat>
-      <v-list dense>
-        <v-list-item
-          v-for="dataset in datasets.datasets"
-          :key="dataset.id"
-          class="pl-2"
-          link
-          @click="activateDataset(dataset.id)"
-          :class="dataset.id === datasets.activeDataset.id && 'blue lighten-4'"
-        >
-          <v-list-item-content>
-            <v-row>
-              <v-col cols="10">
-                <v-text-field
-                  v-model="dataset.name"
-                  :placeholder="'dataset ' + dataset.id"
-                  hide-details
-                  class="mt-0 pt-0"
-                ></v-text-field>
-              </v-col>
-              <v-col
-                cols="2"
-                class="mt-2"
-                :class="`dataset-count-${dataset.id}`"
-              >
-                {{ dataset.plots.length }}
-              </v-col>
-            </v-row>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-      <!-- TODO: モーダル上でデータセットを選べるようにする -->
-      <v-btn small class="mt-2" @click="shouldShowActiveDataset = true"
-        >View Datasets</v-btn
+    <v-list dense height="20vh" class="overflow-y-auto mb-1 mt-1 pa-0" outlined>
+      <v-list-item
+        height="20vh"
+        v-for="dataset in datasets.datasets"
+        :key="dataset.id"
+        class="pl-2"
+        link
+        @click="activateDataset(dataset.id)"
+        :class="dataset.id === datasets.activeDataset.id && 'blue lighten-4'"
       >
-      <!-- <v-btn small v-if="exportBtnText" class="ml-2 mt-2" @click="click">{{
-        exportBtnText
-      }}</v-btn> -->
-    </v-card>
-    <v-dialog
-      v-model="shouldShowActiveDataset"
-      origin="center"
-      scrollable
-      max-width="800px"
-    >
-      <v-card height="80vh">
-        <v-card-title>
-          <span class="text-h5">View Datasets</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="3">
-                <h3>Datasets</h3>
-                <v-list dense>
-                  <v-list-item
-                    v-for="dataset in datasets.datasets"
-                    :key="dataset.id"
-                    class="pl-2"
-                    link
-                    @click="activateDataset(dataset.id)"
-                    :class="
-                      dataset.id === datasets.activeDataset.id &&
-                      'blue lighten-4'
-                    "
-                  >
-                    <v-list-item-content>
-                      <v-list-item-title
-                        v-text="dataset.name"
-                      ></v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-list>
-              </v-col>
-              <v-col cols="6">
-                <clipboard
-                  :sortKey="sortKey"
-                  :sortOrder="sortOrder"
-                  :exportBtnText="exportBtnText"
-                  :exportBtnClick="exportBtnClick"
-                ></clipboard>
-              </v-col>
-              <v-col cols="3">
-                <h3>Sort</h3>
-                <v-select
-                  v-model="sortKey"
-                  :items="sortKeys"
-                  label="sort key"
-                ></v-select>
-                <v-select
-                  v-model="sortOrder"
-                  :items="sortOrders"
-                  label="sort order"
-                ></v-select>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn @click="shouldShowActiveDataset = false"> Close </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+        <v-list-item-content>
+          <v-row>
+            <v-col cols="10">
+              <v-text-field
+                v-model="dataset.name"
+                :placeholder="'dataset ' + dataset.id"
+                hide-details
+                dense
+                class="mt-0 pt-0"
+              ></v-text-field>
+            </v-col>
+            <v-col
+              cols="2"
+              class="pa-0 d-flex align-items-center"
+              :class="`dataset-count-${dataset.id}`"
+            >
+              <span class="align-self-center">
+                {{ dataset.plots.length }}
+              </span>
+            </v-col>
+          </v-row>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+    <!-- TODO: モーダル上でデータセットを選べるようにする -->
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import Clipboard from '@/components/Export/Clipboard.vue'
 import { datasetMapper } from '@/store/modules/dataset'
 import { canvasMapper } from '@/store/modules/canvas'
 
 export default Vue.extend({
-  components: {
-    Clipboard,
-  },
+  components: {},
   data() {
     return {
-      shouldShowActiveDataset: false,
       sortKey: 'as added',
       sortKeys: ['as added', 'x', 'y'],
       sortOrder: 'ascending',
