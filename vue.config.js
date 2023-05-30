09b315d7bca2627526037d7e8feca3e48b7e58f0
MATO Tomoya <tomoya.matou@gmail.com>
2023-05-30 22:00:44 +0900

    Fix/build error (#13)
    * fix: added PostCSS for parsing css of handsontable
    
    * fix: remove chainWebpack since it's unused property and it causes error while yarn serve
    
    * fix: handle optional chaining

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,4 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
-  chainWebpack: (config) => {
-    config.plugin('fork-ts-checker').tap((args) => {
-      args[0].memoryLimit = 4096
-      return args
-    })
-  },
   publicPath: './',
 }
