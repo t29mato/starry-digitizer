1a0c6222ab11f9419d5a4ca2910c1d105b047611
t29mato <tomoya.matou@gmail.com>
2023-05-30 12:41:45 +0900

    fix: values are calculated even when the XY axis is not perpendicular.

--- src/domains/XYAxesCalculator.ts
+++ src/domains/XYAxesCalculator.ts
@@ -10,31 +10,7 @@ export default class XYAxesCalculator {
     this.#axes = axes
     this.#isLog = isLog
   }
-  calculateXYValues(x: number, y: number): { xV: string; yV: string } {
-    // INFO: 点x1と点x2を通る直線が、点tと垂直に交わる点の(x,y)値を計算
-    const calculateVerticalCrossPoint = (
-      x1x: number,
-      x1y: number,
-      x2x: number,
-      x2y: number,
-      tx: number,
-      ty: number
-    ): { x: number; y: number } => {
-      const isParallel = x2y - x1y === 0
-      const isVertical = x2x - x1x === 0
-      const x = isParallel
-        ? tx
-        : (x2x * x1y * (x1y - x2y - ty) +
-            x1x * x2y * (x2y - x1y - ty) +
-            tx * (x2x - x1x) ** 2 +
-            ty * (x1x * x1y + x2x * x2y)) /
-          ((x2x - x1x) ** 2 + (x2y - x1y) ** 2)
-      // INFO: a1 = x1x, a2 = x2x, b1 = x1y, b2 = x2y
-      const y = isVertical
-        ? ty
-        : ((x2y - x1y) * x + x2x * x1y - x1x * x2y) / (x2x - x1x)
-      return { x, y }
-    }
+  calculateXYValues(xt: number, yt: number): { xV: string; yV: string } {
     if (
       !(
         this.#axes.x1.coord &&
@@ -52,7 +28,7 @@ export default class XYAxesCalculator {
       return { xV: 'NaN', yV: 'NaN' }
     }
 
-    const [x1x, x1y, x2x, x2y, x1v, x2v, y1x, y1y, y2x, y2y, y1v, y2v] = [
+    const [xa, ya, xb, yb, a, b, xc, yc, xd, yd, c, d] = [
       this.#axes.x1.coord.xPx,
       this.#axes.x1.coord.yPx,
       this.#axes.x2.coord.xPx,
@@ -66,22 +42,28 @@ export default class XYAxesCalculator {
       this.#axes.y1.value,
       this.#axes.y2.value,
     ]
-    const xPx = calculateVerticalCrossPoint(x1x, x1y, x2x, x2y, x, y).x
-    const yPx = calculateVerticalCrossPoint(y1x, y1y, y2x, y2y, x, y).y
+    const xab = xb - xa
+    const yab = yb - ya
+    const xcd = xd - xc
+    const ycd = yd - yc
+    const r = ((yt - ya) * xcd - (xt - xa) * ycd) / (yab * xcd - xab * ycd)
+    const s = ((yt - ya) * xab - (xt - xa) * yab) / (ycd * xab - xcd * yab)
+    const xp = xa + r * xab
+    const yq = yc + s * ycd
     const xV = this.#isLog.x
       ? Math.pow(
           10,
-          ((xPx - x1x) / (x2x - x1x)) * (Math.log10(x2v) - Math.log10(x1v)) +
-            Math.log10(x1v)
+          ((xp - xa) / (xb - xa)) * (Math.log10(b) - Math.log10(a)) +
+            Math.log10(a)
         )
-      : ((xPx - x1x) / (x2x - x1x)) * (x2v - x1v) + x1v
+      : ((xp - xa) / (xb - xa)) * (b - a) + a
     const yV = this.#isLog.y
       ? Math.pow(
           10,
-          ((yPx - y1y) / (y2y - y1y)) * (Math.log10(y2v) - Math.log10(y1v)) +
-            Math.log10(y1v)
+          ((yq - yc) / (yd - yc)) * (Math.log10(d) - Math.log10(c)) +
+            Math.log10(c)
         )
-      : ((yPx - y1y) / (y2y - y1y)) * (y2v - y1v) + y1v
+      : ((yq - yc) / (yd - yc)) * (d - c) + c
     const xEffectiveDigits = this.calculateEffectiveDigits(
       this.#axes.x2.value,
       this.#axes.x1.value
