c0d9058ae4dd14c6f8eb6d4eafa3ec950c2d505d
Yu Takada <yu.smc.roundabout@gmail.com>
2023-08-10 12:54:55 +0900

    add vue compat settings

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
   publicPath: './',
+
+  //Vue3 移行ビルド(vue-compat)の設定
+  chainWebpack: (config) => {
+    config.resolve.alias.set('vue', '@vue/compat')
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2,
+            },
+          },
+        }
+      })
+  },
 }
