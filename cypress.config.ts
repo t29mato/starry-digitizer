2892020d92cdc42527dee48284908e28b558559e
Yu Takada <yu.smc.roundabout@gmail.com>
2023-09-21 11:45:52 +0900

    add bun and fix cypress base url

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   viewportWidth: 1280,
   viewportHeight: 700,
   e2e: {
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'http://127.0.0.1:8080',
     // setupNodeEvents(on, config) {
     //   // implement node event listeners here
     // },
