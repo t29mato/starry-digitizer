6865e9e895c2d1b4f1ce1e6eff4358defaede3ec
Yu Takada <yu.smc.roundabout@gmail.com>
2023-09-25 15:51:38 +0900

    library buildの検証

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import path, { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +12,26 @@ export default defineConfig({
   },
   server: {
     port: 8080,
+  },
+  build: {
+    lib: {
+      // 複数のエントリーポイントのディクショナリや配列にもできます
+      entry: resolve(__dirname, 'lib/main.ts'),
+      name: 'MyLib',
+      // 適切な拡張子が追加されます
+      fileName: 'my-lib',
+    },
+    rollupOptions: {
+      // ライブラリにバンドルされるべきではない依存関係を
+      // 外部化するようにします
+      external: ['vue'],
+      output: {
+        // 外部化された依存関係のために UMD のビルドで使用する
+        // グローバル変数を提供します
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
   },
 })
