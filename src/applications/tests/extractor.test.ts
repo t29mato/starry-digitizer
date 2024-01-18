f7bfbc04118d925277c4a4ce3df602636c90d84c
Ajitama <yu.smc.roundabout@gmail.com>
2024-01-18 18:06:12 +0900

    Fix unit test (#105)
    * feat: move application service interfaces to to dependent folder
    
    * feat: move extractor domain to application service
    
    * remove: unnecessary files
    
    * 単体テストの落ちていた箇所を修正

---
+++ src/applications/tests/extractor.test.ts
@@ -0,0 +1,7 @@
+import { Extractor } from '../services/extractor'
+import SymbolExtractByArea from '../strategies/extractStrategies/symbolExtractByArea'
+
+test('DO write extractor test', () => {
+  const extractor = Extractor.getInstance()
+  extractor.setStrategy(new SymbolExtractByArea())
+})
