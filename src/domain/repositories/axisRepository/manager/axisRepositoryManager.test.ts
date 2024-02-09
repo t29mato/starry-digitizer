7e7a8934a6cf44d7af91620aefd740d4824b4620
Yu Takada <yu.smc.roundabout@gmail.com>
2024-02-09 14:30:44 +0900

    fix: test

--- src/domain/repositories/axisRepository/manager/axisRepositoryManager.test.ts
+++ src/domain/repositories/axisRepository/manager/axisRepositoryManager.test.ts
@@ -1,22 +1,24 @@
 import { AxisRepositoryInterface } from '../axisRepositoryInterface'
 import { AxisRepositoryManager } from '../manager/axisRepositoryManager'
 
+const axisRepositoryManager = new AxisRepositoryManager()
 let firstInstance: AxisRepositoryInterface
 
 describe('AxisRepositoryManager instance returning', () => {
   beforeEach(() => {
-    firstInstance = axisRepository
+    firstInstance = axisRepositoryManager.getInstance()
   })
 
   test('The instance returned by getInstance() is the same as firstInstance', () => {
-    const secondeInstance: AxisRepositoryInterface = axisRepository
+    const secondeInstance: AxisRepositoryInterface =
+      axisRepositoryManager.getInstance()
 
     expect(secondeInstance === firstInstance).toBe(true)
   })
 
   test('The instance returned by getNewInstance() is NOT the same as firstInstance', () => {
     const secondeInstance: AxisRepositoryInterface =
-      AxisRepositoryManager.getNewInstance()
+      axisRepositoryManager.getNewInstance()
 
     expect(secondeInstance === firstInstance).toBe(false)
   })
