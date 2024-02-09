18e96be8320480c747a004c75c7d4eaec746e9eb
Ajitama <yu.smc.roundabout@gmail.com>
2024-02-09 18:26:21 +0900

    Improve magnifier visibility (#136)
    * feat: increase magnifier size
    
    * feat: add white outline on the center of magnifier

--- src/application/services/magnifier/magnifier.ts
+++ src/application/services/magnifier/magnifier.ts
@@ -4,7 +4,7 @@ export class Magnifier implements MagnifierInterface {
   scale = 5
   magnifierSettingError = ''
   crosshairSizePx = 1
-  sizePx = 200
+  sizePx = 300
 
   private static instance: MagnifierInterface
 
