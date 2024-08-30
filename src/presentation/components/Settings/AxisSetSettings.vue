46206297d158a9f91d61992eb8562542983e285f
Yu Takada <yu.smc.roundabout@gmail.com>
2024-08-30 12:37:12 +0900

    feat: make axis set settings reactive on vue features

--- src/presentation/components/Settings/AxisSetSettings.vue
+++ src/presentation/components/Settings/AxisSetSettings.vue
@@ -6,7 +6,7 @@
           <td class="pl-0 pr-1">X</td>
           <td class="pl-0 pr-1">
             <v-text-field
-              v-model="x1"
+              v-model="x1Axis.value"
               id="x1-value"
               type="number"
               hide-details
@@ -15,7 +15,7 @@
             >
               <div
                 class="c__AxisSetRepository-settings__log-adjuster"
-                v-if="xIsLog"
+                v-if="axisSetRepository.activeAxisSet.xIsLog"
               >
                 <button
                   size="x-small"
@@ -38,7 +38,7 @@
           </td>
           <td class="pl-0 pr-1">
             <v-text-field
-              v-model="x2"
+              v-model="x2Axis.value"
               id="x2-value"
               type="number"
               hide-details
@@ -47,7 +47,7 @@
             >
               <div
                 class="c__AxisSetRepository-settings__log-adjuster"
-                v-if="xIsLog"
+                v-if="axisSetRepository.activeAxisSet.xIsLog"
               >
                 <button
                   id="multiply-by-ten-x2"
@@ -71,7 +71,7 @@
           <td>
             <v-checkbox
               color="primary"
-              v-model="xIsLog"
+              v-model="axisSetRepository.activeAxisSet.xIsLog"
               id="x-is-log"
               hide-details
               density="compact"
@@ -83,7 +83,7 @@
           <td class="pl-0 pr-1">Y</td>
           <td class="pl-0 pr-1">
             <v-text-field
-              v-model="y1"
+              v-model="y1Axis.value"
               id="y1-value"
               type="number"
               hide-details
@@ -92,7 +92,7 @@
             >
               <div
                 class="c__AxisSetRepository-settings__log-adjuster"
-                v-if="yIsLog"
+                v-if="axisSetRepository.activeAxisSet.yIsLog"
               >
                 <button
                   id="multiply-by-ten-y1"
@@ -115,7 +115,7 @@
           </td>
           <td class="pl-0 pr-1">
             <v-text-field
-              v-model="y2"
+              v-model="y2Axis.value"
               id="y2-value"
               type="number"
               hide-details
@@ -124,7 +124,7 @@
             >
               <div
                 class="c__AxisSetRepository-settings__log-adjuster"
-                v-if="yIsLog"
+                v-if="axisSetRepository.activeAxisSet.yIsLog"
               >
                 <button
                   id="multiply-by-ten-y2"
@@ -148,7 +148,7 @@
           <td>
             <v-checkbox
               color="primary"
-              v-model="yIsLog"
+              v-model="axisSetRepository.activeAxisSet.yIsLog"
               id="y-is-log"
               density="compact"
               hide-details
@@ -200,102 +200,80 @@ export default defineComponent({
   computed: {
     errorMessage(): string {
       if (this.axisSetRepository.activeAxisSet.xIsLog) {
-        if (this.x1 === '0' || this.x2 === '0') {
+        if (this.x1Axis.value === 0 || this.x2Axis.value === 0) {
           return 'x1 or x2 should not be 0'
         }
       } else {
-        if (this.x1 === this.x2) {
+        if (this.x1Axis.value === this.x2Axis.value) {
           return 'x1 and x2 should not be same value'
         }
       }
       if (this.axisSetRepository.activeAxisSet.yIsLog) {
-        if (this.y1 === '0' || this.y2 === '0') {
+        if (this.y1Axis.value === 0 || this.y2Axis.value === 0) {
           return 'y1 or y2 should not be 0'
         }
       } else {
-        if (this.y1 === this.y2) {
+        if (this.y1Axis.value === this.y2Axis.value) {
           return 'y1 and y2 should not be same value'
         }
       }
       return ''
     },
+    x1Axis() {
+      return this.axisSetRepository.activeAxisSet.x1
+    },
+    x2Axis() {
+      return this.axisSetRepository.activeAxisSet.x2
+    },
+    y1Axis() {
+      return this.axisSetRepository.activeAxisSet.y1
+    },
+    y2Axis() {
+      return this.axisSetRepository.activeAxisSet.y2
+    },
   },
   data() {
     return {
       axisSetRepository,
-      x1: '0',
-      x2: '1',
-      y1: '0',
-      y2: '1',
-      xIsLog: false,
-      yIsLog: false,
     }
   },
 
   methods: {
     multiplyByTenX1() {
-      this.x1 = String(this.multiplyByTen(parseFloat(this.x1)))
+      this.x1Axis.value = this.multiplyByTen(this.x1Axis.value)
     },
     divideByTenX1() {
-      this.x1 = String(this.divideByTen(parseFloat(this.x1)))
+      this.x1Axis.value = this.divideByTen(this.x1Axis.value)
     },
     multiplyByTenX2() {
-      this.x2 = String(this.multiplyByTen(parseFloat(this.x2)))
+      this.x2Axis.value = this.multiplyByTen(this.x2Axis.value)
     },
     divideByTenX2() {
-      this.x2 = String(this.divideByTen(parseFloat(this.x2)))
+      this.x2Axis.value = this.divideByTen(this.x2Axis.value)
     },
     multiplyByTenY1() {
-      this.y1 = String(this.multiplyByTen(parseFloat(this.y1)))
+      this.y1Axis.value = this.multiplyByTen(this.y1Axis.value)
     },
     divideByTenY1() {
-      this.y1 = String(this.divideByTen(parseFloat(this.y1)))
+      this.y1Axis.value = this.divideByTen(this.y1Axis.value)
     },
     multiplyByTenY2() {
-      this.y2 = String(this.multiplyByTen(parseFloat(this.y2)))
+      this.y2Axis.value = this.multiplyByTen(this.y2Axis.value)
     },
     divideByTenY2() {
-      this.y2 = String(this.divideByTen(parseFloat(this.y2)))
+      this.y2Axis.value = this.divideByTen(this.y2Axis.value)
     },
     multiplyByTen(value: number) {
       if (value === 0) {
         return 1
       }
-      return (value * 10).toPrecision(1)
+      return value * 10
     },
     divideByTen(value: number) {
       if (value === 0) {
         return 0.1
       }
-      return (value * 0.1).toPrecision(1)
-    },
-  },
-  mounted() {
-    this.x1 = String(this.axisSetRepository.activeAxisSet.x1.value)
-    this.x2 = String(this.axisSetRepository.activeAxisSet.x2.value)
-    this.y1 = String(this.axisSetRepository.activeAxisSet.y1.value)
-    this.y2 = String(this.axisSetRepository.activeAxisSet.y2.value)
-    this.xIsLog = this.axisSetRepository.activeAxisSet.xIsLog
-    this.yIsLog = this.axisSetRepository.activeAxisSet.yIsLog
-  },
-  watch: {
-    xIsLog(value: boolean) {
-      this.axisSetRepository.activeAxisSet.setXIsLog(value)
-    },
-    yIsLog(value: boolean) {
-      this.axisSetRepository.activeAxisSet.setYIsLog(value)
-    },
-    x1(value: string) {
-      this.axisSetRepository.activeAxisSet.setX1Value(parseFloat(value))
-    },
-    x2(value: string) {
-      this.axisSetRepository.activeAxisSet.setX2Value(parseFloat(value))
-    },
-    y1(value: string) {
-      this.axisSetRepository.activeAxisSet.setY1Value(parseFloat(value))
-    },
-    y2(value: string) {
-      this.axisSetRepository.activeAxisSet.setY2Value(parseFloat(value))
+      return value * 0.1
     },
   },
 })
